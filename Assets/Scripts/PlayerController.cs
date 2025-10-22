b7b5b53e7bfe44c20230031787872ecb8d342db8
LonelyMatt <132096155+LonelyMatt@users.noreply.github.com>
2025-10-21 21:40:56 -0700

    movement successful

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f; 
-    public Rigidbody2D rb;       
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
 
     private Vector2 moveInput;
+    private PlayerInputActions inputActions;
 
-    void Update()
+    private void Awake()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal"); 
-        moveInput.y = Input.GetAxisRaw("Vertical");   
-
-        moveInput = moveInput.normalized;
+        inputActions = new PlayerInputActions();
     }
 
-    void FixedUpdate()
+    private void OnEnable()
     {
-        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+        inputActions.Player.Enable();
+
+        inputActions.Player.Move.performed += OnMovePerformed;
+        inputActions.Player.Move.canceled += OnMoveCanceled;
     }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMovePerformed;
+        inputActions.Player.Move.canceled -= OnMoveCanceled;
+
+        inputActions.Player.Disable();
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move Input: " + moveInput);
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb != null)
+            rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+
 }
