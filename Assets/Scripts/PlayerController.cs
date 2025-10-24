25f7a05394e4e155a57122184c4692f3cfafd118
LonelyMatt <132096155+LonelyMatt@users.noreply.github.com>
2025-10-23 23:33:22 -0700

    y

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,55 +7,77 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
-
+    
     private Vector2 moveInput;
     private PlayerInputActions inputActions;
-
+    
     private void Awake()
     {
         inputActions = new PlayerInputActions();
     }
-
+    
     private void OnEnable()
     {
         inputActions.Player.Enable();
-
         inputActions.Player.Move.performed += OnMovePerformed;
         inputActions.Player.Move.canceled += OnMoveCanceled;
     }
-
+    
     private void OnDisable()
     {
         inputActions.Player.Move.performed -= OnMovePerformed;
         inputActions.Player.Move.canceled -= OnMoveCanceled;
-
         inputActions.Player.Disable();
     }
-
+    
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-
-        if (animator != null)
-        {
-            animator.SetFloat("MoveX", moveInput.x);
-            animator.SetFloat("MoveY", moveInput.y);
-            animator.SetFloat("Speed", moveInput.sqrMagnitude); 
-        }
+        UpdateAnimationParameters();
     }
-
+    
     private void OnMoveCanceled(InputAction.CallbackContext context)
     {
         moveInput = Vector2.zero;
-
+        UpdateAnimationParameters();
+    }
+    
+    private void UpdateAnimationParameters()
+    {
         if (animator != null)
         {
-            animator.SetFloat("MoveX", 0);
-            animator.SetFloat("MoveY", 0);
-            animator.SetFloat("Speed", 0);
+            bool isMoving = moveInput.sqrMagnitude > 0.01f;
+            animator.SetBool("IsMoving", isMoving);
+            
+            // Reset all directional bools
+            animator.SetBool("MovingUp", false);
+            animator.SetBool("MovingDown", false);
+            animator.SetBool("MovingLeft", false);
+            animator.SetBool("MovingRight", false);
+            
+            // Set the appropriate directional bool based on dominant direction
+            if (isMoving)
+            {
+                if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+                {
+                    // Horizontal movement is dominant
+                    if (moveInput.x > 0)
+                        animator.SetBool("MovingRight", true);
+                    else
+                        animator.SetBool("MovingLeft", true);
+                }
+                else
+                {
+                    // Vertical movement is dominant
+                    if (moveInput.y > 0)
+                        animator.SetBool("MovingUp", true);
+                    else
+                        animator.SetBool("MovingDown", true);
+                }
+            }
         }
     }
-
+    
     private void FixedUpdate()
     {
         if (rb != null)
